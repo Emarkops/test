ca85cbba63552748ebd6292c8415751b327c44e4
yannicktaz <yannick.taes@emarkops.com>
2014-01-20 22:24:37 +0100

    'initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alantaya::Application.config.secret_key_base = 'afc121f4a497822fa2e04f26bf0a83a4d0001908d6edbbf97424d98ef03c6f5c8e02898351b38b8adb76184b5a6cabcd0b42f9490494bbf9ffa0efc79a9fe871'
